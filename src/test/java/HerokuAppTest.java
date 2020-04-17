6a6baa5be5cb730211c60a502b613ebe21f552d2
e.astashonak <e.anischyk@gmail.com>
2020-04-17 05:55:56 +0300

    added test "Checkboxes"

--- src/test/java/HerokuAppTest.java
+++ src/test/java/HerokuAppTest.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
@@ -19,6 +20,23 @@ public class HerokuAppTest {
         driver.findElement(By.xpath("//div[@id='elements']/button[@class='added-manually']")).click();
         int numOfElements = driver.findElements(By.xpath("//div[@id='elements']/button[@class='added-manually']")).size();
         Assert.assertEquals(numOfElements, 1, "элемент не удалился");
+        driver.quit();
+    }
+
+    @Test
+    public void checkboxes(){
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("http://the-internet.herokuapp.com/checkboxes");
+        WebElement element1 = driver.findElement(By.xpath("//form[@id='checkboxes']/input[1]"));
+        Assert.assertFalse(element1.isSelected(), "checkbox1 выбран");
+        element1.click();
+        Assert.assertTrue(element1.isSelected(), "checkbox1 не выбран");
+        WebElement element2 = driver.findElement(By.xpath("//form[@id='checkboxes']/input[2]"));
+        Assert.assertTrue(element2.isSelected(), "checkbox2 не выбран");
+        element2.click();
+        Assert.assertFalse(element2.isSelected(), "checkbox2 выбран");
         driver.quit();
     }
 
